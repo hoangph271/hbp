fa728361f31ba19e233d825849c636a89672c5ed
Phan Huy Hoang <hoangph271@gmail.com>
2022-03-11 01:03:20 -0500

    [Add] EnvKey::MarpApiRoot

--- src/utils/env.rs
+++ src/utils/env.rs
@@ -1,11 +1,13 @@
 pub enum EnvKey {
     JwtSecret,
     AppName,
+    MarpApiRoot
 }
 
 pub fn from_env(env_key: EnvKey) -> &'static str {
     match env_key {
         EnvKey::AppName => dotenv!("APP_NAME"),
         EnvKey::JwtSecret => dotenv!("JWT_SECRET"),
+        EnvKey::MarpApiRoot => dotenv!("MARP_API_ROOT")
     }
 }
